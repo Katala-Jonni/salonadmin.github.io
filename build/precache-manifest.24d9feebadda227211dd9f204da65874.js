a961182909240384a2489b74af814706bc9cc831
Katala-Jonni <35872192+Katala-Jonni@users.noreply.github.com>
2020-05-19 04:52:55 +0300

    Add files via upload

---
+++ build/precache-manifest.24d9feebadda227211dd9f204da65874.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c84f1f5713d943872161",
+    "url": "/material-dashboard-react/static/css/main.aa28b439.chunk.css"
+  },
+  {
+    "revision": "c84f1f5713d943872161",
+    "url": "/material-dashboard-react/static/js/main.c84f1f57.chunk.js"
+  },
+  {
+    "revision": "b8556c08b70d6c956861",
+    "url": "/material-dashboard-react/static/js/runtime~main.b8556c08.js"
+  },
+  {
+    "revision": "d58e2acca2f2f1eee8fb",
+    "url": "/material-dashboard-react/static/css/2.1e04ebd6.chunk.css"
+  },
+  {
+    "revision": "d58e2acca2f2f1eee8fb",
+    "url": "/material-dashboard-react/static/js/2.d58e2acc.chunk.js"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/material-dashboard-react/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/material-dashboard-react/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/material-dashboard-react/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/material-dashboard-react/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "c45df27250a429605793ef20177f5671",
+    "url": "/material-dashboard-react/static/media/logo_solntseva.c45df272.png"
+  },
+  {
+    "revision": "905df581ec629e55445b2902619cb48a",
+    "url": "/material-dashboard-react/index.html"
+  }
+];
